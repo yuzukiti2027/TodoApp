7d2ed2a7470976f6ed772eb33e4418c939c0d1cb
toshi468 <fu.nobuchika0809@gmail.com>
2025-09-02 19:24:05 +0900

    Firebase Setting Files

--- src/config.ts
+++ src/config.ts
@@ -1,21 +1,26 @@
+//firebaseの設定
+
 import { initializeApp } from "firebase/app";
 import {initializeAuth, getReactNativePersistence} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import  ReactNativeAsyncStorage  from "@react-native-async-storage/async-storage";
 
 // Your web app's Firebase configuration
+//process.envで参照できる(実行結果は変わらないけど、追跡を隠せる(つまりenvにかくと隠ぺいされて〇))
 const firebaseConfig = {
-    apiKey: "AIzaSyBGI9MMrWj04PKi2eWetlJ9FoEIyVX6HlM",
-    authDomain: "todoapp-1c714.firebaseapp.com",
-    projectId: "todoapp-1c714",
-    storageBucket: "todoapp-1c714.firebasestorage.app",
-    messagingSenderId: "572282586963",
-    appId: "1:572282586963:web:83c9f126911507146703a6"
+    apiKey: process.env.EXPO_PUBLIC_FB_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_FB_AUTH_DOMAIN,
+    projectId: process.env.EXPO_PUBLIC_FB_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_FB_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_FB_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_FB_APP_ID
 };
 
+//initializeAppでappやauthを初期化して使えるようにしてる
+//そして他のファイルで使えるようにexport
 const app = initializeApp(firebaseConfig);
 const auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(ReactNativeAsyncStorage);
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
 })
 
 const db = getFirestore(app);
